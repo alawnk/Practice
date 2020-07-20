008166486291f77f91f6affae6f5ae133fb2de27
alawnk <45960454+alawnk@users.noreply.github.com>
2020-07-21 00:07:03 +0800

    Update port_value.py
    123

--- port_value.py
+++ port_value.py
@@ -1,36 +1,23 @@
-import os
-import openpyxl
-PATH = '/Users/alawn/Desktop/result-1/'
-for root,dir,files in os.walk(PATH):
-    for file in files:
-        # print(file)
-        workbook  = openpyxl.load_workbook(PATH+file)
-        sheets = workbook.sheetnames
-        for i in range (len(sheets)):
-            sheet = workbook[sheets[i]]
-            port=sheet['A2'].value
-            # print(port)
-            row = sheet.max_row
-            col = sheet.max_column
-            input = []
-            output =[]
-            for n in range (2,row+1):
-                input_value = sheet.cell(row=n,column = 3).value
-                input.append(input_value)
-                output_value = sheet.cell(row=n,column=4).value
-                output.append(output_value)
-def write_excel():
-    port_excel = openpyxl.load_workbook('/Users/alawn/Desktop/Port.xlsx')
-    work_port = port_excel['Sheet1']
-    v = 0
-    work_port.cell(row=n,column=1,value=file)
-    work_port.cell(row=n,column=2,value=port)
-    work_port.cell(row=n,column=3,value=max(input))
-    work_port.cell(row=n,column=4,value=max(output))
-    v += 1
-    port_excel.save('Port.xlsx')
-write_excel()
-
+import os,openpyxl
+def io_value_new():
+    port_value_new = 'C:/Users/alawn/Desktop/result_new/'
+    for root,dir,files in os.walk(port_value_new):
+        for file_name in files:
+            workbook = openpyxl.load_workbook(port_value_new + file_name)
+            sheets = workbook.sheetnames
+            for i in range(len(sheets)):
+                sheet = workbook[sheets[i]]
+                row = sheet.max_row
+                input=[]
+                output=[]
+                for n in range (2,row):
+                    port_num = sheet.cell(row=n,column=1).value
+                    input_value = sheet.cell(row=n, column=3).value
+                    input.append(input_value)
+                    output_value = sheet.cell(row=n, column=4).value
+                    output.append(output_value)
+                print(file_name,port_num,max(input),max(output))
+io_value_new()
 
 
 
