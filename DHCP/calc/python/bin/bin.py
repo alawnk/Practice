7b80ab3e1321162e41111ee66820de9750c27e20
alawnk <45960454+alawnk@users.noreply.github.com>
2023-09-09 11:46:03 +0800

    Delete DHCP directory

--- DHCP/calc/python/bin/bin.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASEDIR)
-
-from my_module import main
-if __name__ == '__main__':
-    main.run()
-    print(__name__)
-    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
--- DHCP/calc/python/my_module/main.py
+++
@@ -1,5 +0,0 @@
-from my_module import calc
-def run ():
-    print(calc.add(3, 8))
-
-print(__name__)
